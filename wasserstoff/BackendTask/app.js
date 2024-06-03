fae0b3aaf5e4b4bc659bf6503336ec10f1e2acb6
Nikhil Gupta <nikhilg7404@gmail.com>
2024-06-03 22:49:19 +0530

    Create app.js Server

---
+++ wasserstoff/BackendTask/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+const { handleRequest } = require('./controllers/requestController');
+
+const app = express();
+const PORT = 4000;
+
+// Middleware to parse JSON requests
+app.use(express.json());
+
+// Enable CORS for all routes
+app.use(cors());
+
+// Route to handle requests
+app.get('/api/request', handleRequest);
+
+app.listen(PORT, () => {
+  console.log(`Load balancer running on port ${PORT}`);
+});

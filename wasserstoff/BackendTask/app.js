b6ffa903b1f56abbfda18e66b5cc84af86a51bac
Nikhil Gupta <nikhilg7404@gmail.com>
2024-06-04 21:09:44 +0530

    Update app.js

--- wasserstoff/BackendTask/app.js
+++ wasserstoff/BackendTask/app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const { handleRequest } = require('./controllers/apiController');
+const cors = require('cors');
 const app = express();
 const PORT = 4000;
 
 // Middleware to parse JSON requests
 app.use(express.json());
+
+app.use(cors());
 
 // Route for handling requests
 app.post('/api/request', handleRequest);

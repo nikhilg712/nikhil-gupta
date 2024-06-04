b3d5e9768dddea0aea7ad1e34b1fb4a7abd0893e
Nikhil Gupta <nikhilg7404@gmail.com>
2024-06-04 18:51:41 +0530

    Update app.js

--- wasserstoff/BackendTask/app.js
+++ wasserstoff/BackendTask/app.js
@@ -1,18 +1,15 @@
-// app.js
 const express = require('express');
-const cors = require('cors');
 const { handleRequest } = require('./controllers/apiController');
 const app = express();
 const PORT = 4000;
 
 // Middleware to parse JSON requests
 app.use(express.json());
-app.use(cors());
 
-// Route to handle incoming API requests
-app.all('/api/request', handleRequest);
+// Route for handling requests
+app.post('/api/request', handleRequest);
+app.get('/api/request', handleRequest);
 
-// Start the load balancer server
 app.listen(PORT, () => {
   console.log(`Load balancer running on port ${PORT}`);
 });
